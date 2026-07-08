5163b478f9fdb01e2311ca27533daf0178a4fd5e
Michal Zagdanski <michal.mzagdanski@gmail.com>
2026-07-08 10:42:50 +0200

    refactor: rewrote all components from memory

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
 
-
-function Stats({ stats }) {
-   
+function Stats ({stats}) {
     return (
-        <div>
-            <h2>Stats</h2>
-
+    <div>
+        <h2>Stats</h2>
             <p>Total sessions: {stats.totalSessions}</p>
             <p>Total hours worked: {stats.totalHoursWorked.toFixed(2)}h</p>
-            <p>Total earnings: £{stats.totalEarnings.toFixed(2)}</p>
-            <p>Earnings Per Hour: £{stats.earningsPerHour.toFixed(2)}</p>
-
-        </div>
-    )
+            <p>Total earnings:£{stats.totalEarnings.toFixed(2)}</p>
+            <p>Earnings per hour: £{stats.earningsPerHour.toFixed(2)}</p>
+        
+    </div>)
 }
-
-
-
 
 
 export default Stats
